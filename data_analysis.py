c9152079a88e232b608f7e1022ae8ca28e3bb6bb
Eduardo Nevess <edu.seven2000@gmail.com>
2022-10-31 17:08:14 +0000

    quase retirar outliers

--- data_analysis.py
+++ data_analysis.py
@@ -48,20 +48,38 @@ t_mag = np.sqrt(np.add(np.square(array[:,7]),
 
 # act1 = t_acc[array[:,-1]==5]
 act = (array[:,-1]==i)
-print(t_acc[act])
-print(np.array([t_acc[act]]))
 
-activities = np.arange(1, 17)
+activities = np.arange(1, 17) # nao sei se util
 box_array = [] # TODO ver se dá para fazer isto com np.array
 for i in range(1,17):
     act = (array[:,-1]==i)
-    #box_array.append(t_acc[act])
-    box_array.append(t_acc[act])
-    print(box_array)
+    box_array.append(t_acc[act]) # usar list porque colunas tem tamanhos diferentes
+    #box_array = [box_array, t_acc[act]]
 
+#box_array = np.array(box_array)
 plt.figure()
 plt.boxplot(box_array, 0, 'gD')
+plt.title("Acelerómetro")
 
+
+for i in range(0,activities.size):
+    q1 = np.quantile(box_array[:][i], 0.25)
+    q3 = np.quantile(box_array[:][i], 0.75)
+    med = np.median(box_array[:][i])
+    
+    iqr = q3-q1
+
+    upper_bound = q3+(1.5*iqr)
+    lower_bound = q1-(1.5*iqr)
+    print(iqr, upper_bound, lower_bound)
+
+    outliers = box_array[:][i][(box_array[:][i] <= lower_bound) | (box_array[:][i] >= upper_bound)]
+    print('The following are the outliers in the boxplot:{}'.format(outliers))
+    box_array[:][i] = box_array[:][i][(box_array[:][i] >= lower_bound) & (box_array[:][i] <= upper_bound)]
+    
+plt.figure(figsize=(12, 7))
+plt.boxplot(box_array)
+plt.show()
 
 '''
 fig, ax = plt.subplots(3, 1)
